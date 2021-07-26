db1ca2a12bee1b1496114e900430170679206ff9
Michael-Dyq <68020340+Michael-Dyq@users.noreply.github.com>
2021-07-26 16:41:10 -0400

    delele while

--- run-events-dyq.sh
+++ run-events-dyq.sh
@@ -3,8 +3,7 @@
 #export GRB_LICENSE_FILE= /shared/ccgadmin/app/gurobi/gurobi_client.lic
 #export GRB_LICENSE_FILE= /shared/ccgadmin/app/gurobi/gurobi.lic
 export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/EventExtraction-yuqian/gurobi_client_server.lic
-while [ 1 -eq 1 ]
-do 
-	python backend.py
-done
+
+python backend.py
+
 
