323dc94dd8fb2e3e113f1edb6f631b9560cefb1f
Yuqian Deng <68020340+Michael-Dyq@users.noreply.github.com>
2021-07-26 16:14:16 -0400

    Update run-KAIROS-events.sh

--- run-KAIROS-events.sh
+++ run-KAIROS-events.sh
@@ -1,5 +1,8 @@
 #export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi.lic
-export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi_client_server.lic
+#export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi_client_server.lic
+#export GRB_LICENSE_FILE= /shared/ccgadmin/app/gurobi/gurobi_client.lic
+#export GRB_LICENSE_FILE= /shared/ccgadmin/app/gurobi/gurobi.lic
+export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/EventExtraction-yuqian/gurobi_client_server.lic
 while [ 1 -eq 1 ]
 do 
 	python backend.py
