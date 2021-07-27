65efd78cca4048b5c0e8314e68e3f362a6b4b13c
Michael-Dyq <68020340+Michael-Dyq@users.noreply.github.com>
2021-07-27 10:56:00 -0400

    temp for temp 4024

---
+++ temp-temp-dyq.sh
@@ -0,0 +1,13 @@
+# export NVIDIA_VISIBLE_DEVICES=-1
+#export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi.lic
+#export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi_client_server.lic
+
+#export GRB_LICENSE_FILE= /shared/ccgadmin/app/gurobi/gurobi.lic
+#export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/EventExtraction-yuqian/gurobi_client_server.lic
+export GRB_LICENSE_FILE= /shared/ccgadmin/app/gurobi/gurobi_client.lic
+export CUDA_VISIBLE_DEVICES=-1;
+
+python server.py 1 4024
+
+
+
