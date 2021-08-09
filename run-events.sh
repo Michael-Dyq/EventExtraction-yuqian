b812fad8f757d50d4d5ddddb06fe8536fa066423
Yuqian Deng <68020340+Michael-Dyq@users.noreply.github.com>
2021-08-08 21:20:28 -0400

    update run-events.sh（gurobi_client.lic）

--- run-events.sh
+++ run-events.sh
@@ -1,9 +1,9 @@
 #export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi.lic
 #export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi_client_server.lic
-#export GRB_LICENSE_FILE= /shared/ccgadmin/app/gurobi/gurobi_client.lic
 #export GRB_LICENSE_FILE= /shared/ccgadmin/app/gurobi/gurobi.lic
 #export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/EventExtraction-yuqian/gurobi_client_server.lic # this one works
-export GRB_LICENSE_FILE= /shared/ccgadmin/app/gurobi/gurobi_docker_client.lic
+#export GRB_LICENSE_FILE= /shared/ccgadmin/app/gurobi/gurobi_docker_client.lic
+export GRB_LICENSE_FILE= /shared/ccgadmin/app/gurobi/gurobi_client.lic
 while [ 1 -eq 1 ]
 do
     python backend.py
